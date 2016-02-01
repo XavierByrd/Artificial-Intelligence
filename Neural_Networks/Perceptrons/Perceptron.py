6a9e9d35a9c2187b2ffa685d9343c12a0b0794dd
Xavier Byrd <xavierbyrd@live.com>
2016-02-01 00:04:17 -0800

    Perceptron Functional
    Perceptron can be trained, there is now a way to evaluate the accuracy
    of the perceptron's guesses. This is in testPerceptron.py

--- Neural_Networks/Perceptrons/Perceptron.py
+++ Neural_Networks/Perceptrons/Perceptron.py
@@ -3,17 +3,45 @@ from random import uniform
 
 class Perceptron(object):
 
-    # Adjusts the rate of learning
-    # Larger constant creates more drastic changes in behavior
-    # Smaller constant creates more gradual, but accurate approximations.
-    LEARNINGCONSTANT = 0.01;
-
     # Constructor
-    def __init__(self, numInputs):
+    # Modified to include a parameter for weights
+    #  -This will allow us to test our newly trained perceptron
+    #   By entering in pre-trained weights.
+    def __init__(self, numInputs, weights):
         self.weights = []
-        
-        for i in range(0, numInputs):
-            self.weights.append(uniform(-1,1))
+
+        # Placeholder for testing
+        self.inputs = []
+
+        # Adjusts the rate of learning
+        # Larger constant creates more drastic changes in behavior
+        # Smaller constant creates more gradual, but accurate approximations.
+        self.LEARNINGCONSTANT = 0.01;
+
+        if weights is not None:
+            self.weights = weights
+        else:
+            for i in range(0, numInputs):
+                self.weights.append(uniform(-1,1))
+
+    # My method to explicitly set the inputs
+    def setInputs(self, inputs):
+        self.inputs = inputs
+
+    # PLAIN ENGLISH (Returns String)
+    # My method to get an explicit determination
+    def getDeterminationInEnglish(self):
+        determination = self.feedforeward(self.inputs)
+        if determination > 0:
+            return "I think that this point is above the line."
+        else:
+            return "I think that this point is below the line."
+
+    # BOOLEAN (Returns Bool)
+    # My method to get an explicit determination
+    def getDeterminationInBoolean(self):
+        determination = self.feedforeward(self.inputs)
+        return True if  determination > 0 else False
 
     def activate(self, sum):
         return 1 if sum > 0 else -1
@@ -26,14 +54,14 @@ class Perceptron(object):
 
     # Trains the neural network against known data
     # Takes inputs and the 'known' answer
-    def train(inputs, desired):
-        guess = feedforeward(inputs)
+    def train(self, inputs, desired):
+        guess = self.feedforeward(inputs)
 
         # Error, the difference between the known answer and the perceptron's guess
         error = desired - guess
 
         # Adjust weights according to error & learning constant
-        for i in range(len(weights)):
+        for i in range(len(self.weights)):
             # New weight is Learning Constant * Error * Delta Weight
             # Delta Weight = Error * Input
-            weights[i] += LEARNINGCONSTANT * error * inputs[i]
+            self.weights[i] += self.LEARNINGCONSTANT * error * inputs[i]
--- Neural_Networks/Perceptrons/testPerceptron.py
+++ Neural_Networks/Perceptrons/testPerceptron.py
@@ -3,10 +3,100 @@
 ###
 
 import logging
+import random
+from pprint import pprint
 from logging import debug
+from logging import info
 from Perceptron import Perceptron
+from Trainer import Trainer
 
 # Output logs to console
 logging.basicConfig(level=logging.DEBUG, format="%(levelname)s - %(message)s")
 
-debug("test")
+debug("Program Start")
+
+# Holder for our training points
+training = []
+TRAININGPOINTS = 2000
+
+# Size of our "view"
+WIDTH = 640
+HEIGHT = 360
+
+# Our formula (To show to the user)
+FORMULA = "y = 2x + 1"
+
+# The formula for our line
+# We are training our perceptron to understand if a point is above or below this
+# Change this equation to train the perceptron for different cases
+def f(x):
+    # Change this to change the perceptrons training regimine
+    return 2*x+1
+
+# Determine if the pont is above or below the line using MATH!
+# Returns True if point is above f(x) returnes false if it is below
+def evaluateIfPointAboveFx(inputs):
+    return False if inputs[1] < f(inputs[0]) else True
+
+# Determine if the perceptron's guess is correct
+# Returns true if the perceptron is correct, and false if it is not
+def evaluatePerceptron(perceptron, answer):
+    return True if perceptron.getDeterminationInBoolean() == answer else False
+
+def setup():
+
+    for i in range(0, TRAININGPOINTS):
+        x = random.uniform(-WIDTH/2, WIDTH/2)
+        y = random.uniform(-HEIGHT/2, HEIGHT/2)
+        # Is the correct answer 1 or -1?
+        answer = 1
+        if y < f(x):
+            answer = -1;
+        training.append(Trainer(x, y, answer))
+
+def runTraining():
+    ptron = Perceptron(3, None)
+    total = 0
+    for i in range(0, TRAININGPOINTS):
+        ptron.train(training[i].inputs, training[i].answer)
+        for j in range(0, TRAININGPOINTS):
+            guess = ptron.feedforeward(training[j].inputs)
+        total = i
+    return ptron
+
+def runTesting():
+    testPoints = [[17,3,1],[22,87,1],[-10,-2,1],[1,1,1], [0, 10, 1], [0, 0, 1], [1, 20, 1]]
+    correctGuesses = 0
+
+    # 1. Set the neuron's inputs
+    # 2. Run f(x) and Perceptron
+    # 3. Compare Perceptron's Guess to the Mathematically Derived Answer
+    print("")
+    print("\t\t\t   ---Printing Results---")
+    header = "Point \t\t\t\t Perceptron \t\t\t Formula: %s " %(FORMULA)
+    print(header)
+    for i in range(0, len(testPoints)):
+        neuron.setInputs(testPoints[i])
+        guessIsCorrect = evaluatePerceptron(neuron, evaluateIfPointAboveFx(testPoints[i]))
+        if(guessIsCorrect):
+            correctGuesses += 1
+        output = "%s \t %s \t %s" %\
+         (testPoints[i],\
+          neuron.getDeterminationInEnglish(),\
+          guessIsCorrect)
+        print(output)
+    print("Perceptron Success: " + str((correctGuesses/len(testPoints)) * 100) + "%")
+    print("")
+    debug("Done.")
+
+debug("Initializing Perceptron")
+setup()
+debug("Finished")
+debug("Running Perceptron Training Simulation...")
+debug("---")
+
+# Setup our final Perceptron for testing
+neuron = runTraining()
+info("Trained Perceptron: " + str(neuron.weights))
+
+runTesting()
